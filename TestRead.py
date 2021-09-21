acadac7936fb336e430022186752d27baac4e210
BaileyPaulK <quill@DESKTOP-LMO064H.localdomain>
2021-09-20 21:50:50 -0400

    tested openpyxl and messed with formatting error involving sheets

--- TestRead.py
+++ TestRead.py
@@ -1,9 +1,14 @@
 from openpyxl import load_workbook
-workbook = load_workbook(filename = "TestData.xlsx")
+workbook = load_workbook(filename = "Data.xlsx")
 sheet = workbook.active
+
+print(workbook.sheetnames)
+
+for x in workbook.sheetnames:
+    print(x)
 
 while True:
     cell = input("Please Input Cell: ")
-    if cell == exit:
+    if cell == "exit":
         break
     print(sheet[cell].value)
