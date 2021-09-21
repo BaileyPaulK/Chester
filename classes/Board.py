043a874a8088972507435ff0c1ce2ddf2cdb332f
BaileyPaulK <quill@DESKTOP-LMO064H.localdomain>
2021-09-20 22:28:56 -0400

    cleaned things up

--- classes/Board.py
+++ classes/Board.py
@@ -1,5 +1,6 @@
 class Board:
-    def __init__(self) -> None:
+    def __init__(self, id) -> None:
+        self.id = id
         self.parts  = []    #note will be array of arrays [part object, qty used per board]
     
     def AddPart(self, part, qty):
